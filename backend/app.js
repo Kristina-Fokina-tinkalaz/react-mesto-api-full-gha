8c78e63d078fce643c877054955652314356ed28
Kristina-Fokina-tinkalaz <100839893+Kristina-Fokina-tinkalaz@users.noreply.github.com>
2023-05-05 12:30:31 +0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -20,8 +20,8 @@ app.get('/crash-test', () => {
 });
 app.use('/cards', auth, require('./routes/cards'));
 app.use('/users', auth, require('./routes/users'));
-app.use('/', require('./routes/auth'));
-app.use(auth, () => {
+
+app.use(() => {
   throw new NotFoundError('Такой страницы не существует');
 });
 app.use(errors());
