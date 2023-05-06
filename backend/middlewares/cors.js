c7b07232c8462c9054a531f74c6b81bca51ab7a6
Kristina Fokina <kristinalazareva1993@yandex.ru>
2023-05-06 16:31:10 +0700

    kkk1

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -7,7 +7,7 @@ const allowedCors = [
 module.exports = (req, res, next) => {
     const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', "*");
     const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers']; 
   const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE"; 
