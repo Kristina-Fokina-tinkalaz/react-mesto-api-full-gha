b616b5c9145af552c9b85d469b3c329a8baf440c
Kristina Fokina <kristinalazareva1993@yandex.ru>
2023-05-07 09:21:27 +0700

    hhддhlgfjl

--- backend/app.js
+++ backend/app.js
@@ -12,11 +12,41 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
+const allowedCors = [
+  'https://praktikum.tk',
+  'http://praktikum.tk',
+  'localhost:3000',
+  'http://localhost:3000',
+  'http://127.0.0.1:3000',
+  /(https|http)?:\/\/(?:www\.|(?!www))mesto-tinkalaz.nomoredomains.monster\/[a-z]+\/|[a-z]+\/|[a-z]+(\/|)/,
+];
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+
+  if (allowedCors.some((e) => e.test && e.test(origin)) || allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  return next();
+});
+
+
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(express.json());
-app.use(cors());
-app.options('*', cors());
+// app.use(cors());
+// app.options('*', cors());
 
 app.get('/crash-test', () => {
   setTimeout(() => {
